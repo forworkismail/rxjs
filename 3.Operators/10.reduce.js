5729a39b32e76ef1355c70c657cb7d871bc3a569
Ismail <forwork.ismail@gmail.com>
2022-04-07 23:17:27 +0430

    Create reduce operator

---
+++ 3.Operators/10.reduce.js
@@ -0,0 +1,7 @@
+const { of } = require('rxjs');
+const { reduce } = require('rxjs/operators');
+
+const source = of(1, 2, 3, 4);
+const example = source.pipe(reduce((acc, val) => acc + val));
+//output: Sum: 10'
+const subscribe = example.subscribe(val => console.log('Sum:', val));
