9acf57c6c056183180c1a49430f42f1c2e7822da
Ismail <forwork.ismail@gmail.com>
2022-04-08 21:23:23 +0430

    Create skip operator

---
+++ 3.Operators/11-20/14.skip.js
@@ -0,0 +1,10 @@
+// Skip the provided number of emitted values
+const { interval } = require('rxjs');
+const { skip } = require('rxjs/operators');
+
+//emit every 1s
+const source = interval(1000);
+//skip the first 5 emitted values
+const example = source.pipe(skip(5));
+//output: 5...6...7...8........
+const subscribe = example.subscribe(val => console.log(val));
