70234d81f51d437ec57ca58cbe3cb0ab9877df5d
Ismail <forwork.ismail@gmail.com>
2022-04-08 22:10:35 +0430

    Create forkJoin operator

---
+++ 3.Operators/11-20/18.forkJoin.js
@@ -0,0 +1,59 @@
+// When all observables complete, emit the last emitted value from each
+// Why use forkJoin?
+// This operator is best used when you have a group of observables and only care about the final emitted value of each. 
+// One common use case for this is if you wish to issue multiple requests on page load (or some other event) and only 
+// want to take action when a response has been received for all.
+const { ajax } = require('rxjs/ajax');
+const { forkJoin,  interval, of } = require('rxjs');
+const { delay, take } = require('rxjs/operators');
+
+/*
+  when all observables complete, provide the last
+  emitted value from each as dictionary
+*/
+forkJoin(
+  // as of RxJS 6.5+ we can use a dictionary of sources
+  {
+    google: ajax.getJSON('https://api.github.com/users/google'),
+    microsoft: ajax.getJSON('https://api.github.com/users/microsoft'),
+    users: ajax.getJSON('https://api.github.com/users')
+  }
+)
+  // { google: object, microsoft: object, users: array }
+  .subscribe(console.log);
+
+  /* -------------------------------------------------------------------------------------- */
+
+
+const myPromise = val =>
+  new Promise(resolve =>
+    setTimeout(() => resolve(`Promise Resolved: ${val}`), 5000)
+  );
+
+/*
+  when all observables complete, give the last
+  emitted value from each as an array
+*/
+const example = forkJoin({
+  //emit 'Hello' immediately
+  sourceOne: of('Hello'),
+  //emit 'World' after 1 second
+  sourceTwo: of('World').pipe(delay(1000)),
+  //emit 0 after 1 second
+  sourceThree: interval(1000).pipe(take(1)),
+  //emit 0...1 in 1 second interval
+  sourceFour: interval(1000).pipe(take(2)),
+  //promise that resolves to 'Promise Resolved' after 5 seconds
+  sourceFive: myPromise('RESULT')
+});
+/*
+ * Output:
+ * { 
+ *   sourceOne: "Hello", 
+ *   sourceTwo: "World", 
+ *   sourceThree: 0,
+ *   sourceFour: 1,
+ *   sourceFive: "Promise Resolved: RESULT"
+ * }
+ */
+const subscribe = example.subscribe(val => console.log(val));
