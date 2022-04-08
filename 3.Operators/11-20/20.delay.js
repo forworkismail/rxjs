34f0a48fd600b3271df50e751d2e90358d0c4c19
Ismail <forwork.ismail@gmail.com>
2022-04-08 22:31:52 +0430

    Create delay operator

---
+++ 3.Operators/11-20/20.delay.js
@@ -0,0 +1,15 @@
+// Delay emitted values by given time
+const { of, merge } = require('rxjs');
+const { mapTo, delay } = require('rxjs/operators');
+
+//emit one item
+const example = of(null);
+//delay output of each by an extra second
+const message = merge(
+  example.pipe(mapTo('Hello')),
+  example.pipe(mapTo('World!'), delay(1000)),
+  example.pipe(mapTo('Goodbye'), delay(2000)),
+  example.pipe(mapTo('World!'), delay(3000))
+);
+//output: 'Hello'...'World!'...'Goodbye'...'World!'
+const subscribe = message.subscribe(val => console.log(val));
