bf4fcb23ada0f1d38b39c7ba77c2992a32401001
Ismail <forwork.ismail@gmail.com>
2022-04-08 22:27:42 +0430

    Create tap operator

---
+++ 3.Operators/11-20/19.tap.js
@@ -0,0 +1,36 @@
+// Transparently perform actions or side-effects, such as logging
+const { of } = require('rxjs');
+const { tap, map } = require('rxjs/operators');
+
+const source = of(1, 2, 3, 4, 5);
+// transparently log values from source with 'tap'
+const example = source.pipe(
+  tap(val => console.log(`BEFORE MAP: ${val}`)),
+  map(val => val + 10),
+  tap(val => console.log(`AFTER MAP: ${val}`))
+);
+
+//'tap' does not transform values
+//output: 11...12...13...14...15
+const subscribe = example.subscribe(val => console.log(val));
+
+/* -------------------------------------------------------------------------------------- */
+
+
+// tap also accepts an object map to log next, error, and complete
+const example2 = source
+  .pipe(
+    map(val => val + 10),
+    tap({
+      next: val => {
+        // on next 11, etc.
+        console.log('on next', val);
+      },
+      error: error => {
+        console.log('on error', error.message);
+      },
+      complete: () => console.log('on complete')
+    })
+  )
+  // output: 11, 12, 13, 14, 15
+  .subscribe(val => console.log(val));
