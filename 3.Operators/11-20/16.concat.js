05cb908028b3593ae92eb2224e41b1557d5dde74
Ismail <forwork.ismail@gmail.com>
2022-04-08 21:31:08 +0430

    Create concat operator

---
+++ 3.Operators/11-20/16.concat.js
@@ -0,0 +1,13 @@
+// Subscribe to observables in order as previous completes
+// RxJS v6+
+const { of, concat } = require('rxjs');
+
+concat(
+  of(1, 2, 3),
+  // subscribed after first completes
+  of(4, 5, 6),
+  // subscribed after second completes
+  of(7, 8, 9)
+)
+  // log: 1, 2, 3, 4, 5, 6, 7, 8, 9
+  .subscribe(console.log);
