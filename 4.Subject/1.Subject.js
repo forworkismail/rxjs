3e7008f2a20235b624c8db217dc5e210d9324c22
Ismail <forwork.ismail@gmail.com>
2022-04-11 21:08:12 +0430

    Create Behavior Subject

--- 4.Subject/1.Subject.js
+++ 4.Subject/1.Subject.js
@@ -1,4 +1,4 @@
-const { Subject } = require('rxjs');
+const { Subject, from } = require('rxjs');
 
 const subject = new Subject();
 
@@ -17,3 +17,29 @@ subject.next(2);
 // observerB: 1
 // observerA: 2
 // observerB: 2
+
+/* ---------------------------------------------------- */
+
+// Since a Subject is an Observer, this also means you may provide a Subject
+// as the argument to the subscribe of any Observable, like the example below shows:
+
+const subject2 = new Subject();
+
+subject2.subscribe({
+  next: (v) => console.log(`observerA: ${v}`),
+});
+subject2.subscribe({
+  next: (v) => console.log(`observerB: ${v}`),
+});
+
+const observable = from([1, 2, 3]);
+
+observable.subscribe(subject2); // You can subscribe providing a Subject
+
+// Logs:
+// observerA: 1
+// observerB: 1
+// observerA: 2
+// observerB: 2
+// observerA: 3
+// observerB: 3
