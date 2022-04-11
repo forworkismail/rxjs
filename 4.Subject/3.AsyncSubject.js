72a1c15578cd213d446c2b19d9e1582200b3db69
Ismail <forwork.ismail@gmail.com>
2022-04-11 21:14:35 +0430

    Solve 3.Async Subject

---
+++ 4.Subject/3.AsyncSubject.js
@@ -0,0 +1,12 @@
+const { AsyncSubject } = require('rxjs');
+
+const sub = new AsyncSubject();
+
+sub.subscribe(console.log);
+
+sub.next(123); //nothing logged
+
+sub.subscribe(console.log);
+
+sub.next(456); //nothing logged
+sub.complete(); //456, 456 logged by both subscribers
